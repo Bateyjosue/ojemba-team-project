4e54901e1ddd175430f4a83b4820c4ffa0756f09
Junior Hirwa <iamhirwejr@gmail.com>
2023-08-17 15:51:27 +0200

    asset: added a new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,8 @@ export default {
         primary: "#8ACC50",
         secondary: "#001342",
         primarydarker: "#116355",
-        darkergray: "#7F7F7F"
+        darkergray: "#7F7F7F",
+        primaryLinear: "#8ACC50"
       }
     },
   },
