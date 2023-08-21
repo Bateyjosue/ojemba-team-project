c27fa8a5a0dff016db93302419cc3dac22d72ada
Mugisha Kevine <Kevinemug>
2023-08-21 16:49:34 +0100

    update: added responsiveness

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const loadComponents = (id, url) =>  {
 window.onload = () => {
     loadComponents('header', 'src/Components/header.html');
     loadComponents('hero', 'src/Components/hero.html');
+loadComponents('icon-section', 'src/Components/sectionWithIcons.html');
     loadComponents('footer', 'src/Components/footer.html');
     loadComponents('button__skillset', 'src/Components/button.html');
     loadComponents('textWithLine', 'src/Components/textWithLine.html');
