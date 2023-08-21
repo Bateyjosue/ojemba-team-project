706918f387c3a395331f9df717e647f238eee458
Bateyjosue <josuebatey19@gmail.com>
2023-08-21 21:27:11 +0200

    feat: expand more and less functionalities

--- src/main.js
+++ src/main.js
@@ -8,15 +8,27 @@ const loadComponents = (id, url) =>  {
     });
 }
 
-
-
 window.onload = () => {
     loadComponents('header', 'src/Components/header.html');
     loadComponents('hero', 'src/Components/hero.html');
-loadComponents('icon-section', 'src/Components/sectionWithIcons.html');
-    loadComponents('footer', 'src/Components/footer.html');
-    loadComponents('button__skillset', 'src/Components/button.html');
-    // loadComponents('textWithLine', 'src/Components/textWithLine.html');
+    loadComponents('icon-section', 'src/Components/sectionWithIcons.html');    // // loadComponents('textWithLine', 'src/Components/textWithLine.html');
     loadComponents('managedTeams', 'src/Components/managedTeams.html')
     loadComponents('kotakt', 'src/Components/kotakt.html');
+    loadComponents('footer', 'src/Components/footer.html');
 }
+
+const kotakt = document.querySelector('main #kotakt');
+
+kotakt.addEventListener('click', (event) => {
+    if (event.target.className === 'plus'){
+        event.target.setAttribute('src', '/src/Assets/media/Minus-icon.svg')
+        event.target.setAttribute('class', 'minus')
+        event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'block'
+        console.log(event.target.parentNode.nextSibling.nextElementSibling.innerHTML)
+    }else if (event.target.className === 'minus'){
+        event.target.setAttribute('src', '/src/Assets/media/Plus-icon.svg')
+        event.target.setAttribute('class', 'plus')
+        event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'none'
+        console.log(event.target.parentNode.nextSibling.nextElementSibling.innerHTML)
+    }
+});
