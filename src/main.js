9ad7e34bd2198dadfbf49f04faece94035479a7f
Mugisha Kevine <Kevinemug>
2023-08-22 17:23:56 +0100

    feat: qualitat section

--- src/main.js
+++ src/main.js
@@ -29,12 +29,23 @@ window.onload = () => {
 
 const expandLessMore = (event) => {
     if (event.target.className === 'plus'){
-        event.target.setAttribute('src', '/src/Assets/media/minus.svg')
+        event.target.setAttribute('src', '/src/Assets/media/Minus-icon.svg')
         event.target.setAttribute('class', 'minus')
         event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'block'
     }else if (event.target.className === 'minus'){
-        event.target.setAttribute('src', '/src/Assets/media/+.png')
+    event.target.setAttribute('src', '/src/Assets/media/Plus-icon.svg')
         event.target.setAttribute('class', 'plus')
+        event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'none'
+    }
+}
+const expandLessMore2 = (event) => {
+    if (event.target.className === 'plus2'){
+        event.target.setAttribute('src', '/src/Assets/media/minus.svg')
+        event.target.setAttribute('class', 'minus2')
+        event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'block'
+    }else if (event.target.className === 'minus2'){
+        event.target.setAttribute('src', '/src/Assets/media/plusI.svg')
+    event.target.setAttribute('class', 'plus2')
         event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'none'
     }
 }
@@ -48,6 +59,6 @@ kotakt.addEventListener('click', (event) => {
 const qualitat = document.querySelector('main #qualitat-section');
 
 qualitat .addEventListener('click', (event) => {
-    expandLessMore(event)
+    expandLessMore2(event)
 });
 
