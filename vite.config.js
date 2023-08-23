828aec23dc8b9c831adc8b47e00604456f8b6712
Josh Batey <josuebatey19@gmail.com>
2023-08-23 12:39:33 +0200

    fix: deployment vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,21 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     rollupOptions: {
-  input: {
-        main: resolve(__dirname, "/index.html"),
+      input: {
         header:resolve(__dirname,"src/Components/header.html"),
         hero:resolve(__dirname,'src/Components/hero.html'),
+        button: resolve(__dirname, 'src/Components/button.html'),
+        cardNameDescription:resolve(__dirname,'src/Components/cardNameDescription.html'),
+        expand:resolve(__dirname,"src/Components/expand.html"),
         maibornWold:resolve(__dirname,"src/Components/maibornWolf.html"),
-    
-        managedTeams:resolve(__dirname,"src/Components/managedTeams.html"),
-        
-        
-        unsere:resolve(__dirname,"src/Components/unsere.html"),
-        ruanda:resolve(__dirname,"src/Components/ruanda.html"),
         kotakt:resolve(__dirname,"src/Components/kotakt.html"),
+        managedTeams:resolve(__dirname,"src/Components/managedTeams.html"),
+        ruanda:resolve(__dirname,"src/Components/ruanda.html"),
+        sectionCardNameDescription:resolve(__dirname,"src/Components/sectionCardNameDescription.html"),
+        sectionWithIcons:resolve(__dirname,"src/Components/sectionWithIcons.html"),
+        skillset:resolve(__dirname,"src/Components/skillset.html"),
+        textWithLine:resolve(__dirname,"src/Components/textWithLine.html"),
+        unsere:resolve(__dirname,"src/Components/unsere.html"),
         footer: resolve(__dirname,"src/Components/footer.html")
       },
     },
