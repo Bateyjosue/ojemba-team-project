fec4bd0bf86467ff90f472cc741a658a164271df
Junior Hirwa <iamhirwejr@gmail.com>
2023-08-28 16:45:55 +0200

    fixed the founders section

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ const expandLessMore = (event) => {
         event.target.setAttribute('src', '/src/Assets/media/Minus-icon.svg')
         event.target.setAttribute('class', 'minus')
         event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'block'
-    }else if (event.target.className === 'minus'){
+    } else if (event.target.className === 'minus'){
     event.target.setAttribute('src', '/src/Assets/media/Plus-icon.svg')
         event.target.setAttribute('class', 'plus')
         event.target.parentNode.nextSibling.nextElementSibling.style['display'] = 'none'
