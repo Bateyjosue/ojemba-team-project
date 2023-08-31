242e86bce865874d1d0fb37d2439cc6615d51044
Bateyjosue <josuebatey19@gmail.com>
2023-08-31 18:48:33 +0200

    hotfix: fix deployment to resolve the vite config

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        header:resolve(__dirname,"src/components/header.html"),
+        header:resolve(__dirname,"./src/components/header.html"),
         hero:resolve(__dirname,'src/components/hero.html'),
         maibornWold:resolve(__dirname,"src/components/maibornWolf.html"),
         kotakt:resolve(__dirname,"src/components/kotakt.html"),
         managedTeams:resolve(__dirname,"src/components/managedTeams.html"),
         ruanda:resolve(__dirname,"src/components/ruanda.html"),
-        sectionCardNameDescription:resolve(__dirname,"src/components/sectionCardNameDescription.html"),
         sectionWithIcons:resolve(__dirname,"src/components/sectionWithIcons.html"),
         unsere:resolve(__dirname,"src/components/unsere.html"),
         footer: resolve(__dirname,"src/components/footer.html"),
